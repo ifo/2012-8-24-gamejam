53e4d359b65b4ca5e438d26c963908d1e5ed0a8f
Steve McCarthy <raygunomix@gmail.com>
2012-08-25 00:13:30 -0400

    added walls and collision checks (no effect from collision), level 2

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ function love.load()
   love.graphics.setMode(600,400,false,true,0)
 
   -- all tables (except keys)
-  g,l,p = {},{},{}
+  g,l,p,w,c = {},{},{},{},{}
 
   -- ground stuff
   g.y = love.graphics.getHeight() - 50
@@ -15,6 +15,16 @@ function love.load()
   -- platform (ledge) stuff
   function l.dr()
     -- no ledges yet
+  end
+  
+  -- wall stuff
+  w.x = 450
+  w.h = 100
+  w.y = g.y - w.h
+  w.w = 2
+
+  function w.dr()
+    love.graphics.rectangle("fill", w.x, w.y, w.w, w.h)
   end
 
   -- quit stuff
@@ -54,6 +64,18 @@ function love.load()
       p.x = 0
     end
   end
+
+  -- collision checks (player, wall, ledge)
+  function c.rectcheck(r1, r2)
+    if r1.x + r1.w < r2.x or
+      r1.x > r2.x + r2.w or
+      r1.y + r1.h < r2.y or
+      r1.y > r2.y + r2.h then
+      return false
+    else
+      return true
+    end
+  end
 end
 
 function love.update(dt)
@@ -71,6 +93,13 @@ end
 function love.draw()
   g.dr()
   p.dr()
+  w.dr()
   l.dr() -- no ledges yet
-  love.graphics.print("move to this edge to win -->", g.ed - 200, 200)
+  love.graphics.print("move to this edge to win -->", g.ed - 200, 100)
+  love.graphics.print('ignore this "wall" -->', w.x - 130, 280)
+  if c.rectcheck(p,w) then
+    love.graphics.print("touching the wall", 15, 15)
+  else
+    love.graphics.print("not touching the wall", 15, 15)
+  end
 end
